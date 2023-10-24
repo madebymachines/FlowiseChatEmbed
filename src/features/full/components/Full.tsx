0b36699af8a672e092a560f1dee87e4ac69b5a45
kiranahani <83271196+kiranahani@users.noreply.github.com>
2023-10-24 15:05:12 +0700

    Update Full.tsx

--- src/features/full/components/Full.tsx
+++ src/features/full/components/Full.tsx
@@ -1,45 +1,40 @@
-import styles from '../../../assets/index.css'
-import { Bot, BotProps } from '@/components/Bot'
-import { BubbleParams } from '@/features/bubble/types'
-import { createSignal, onCleanup, onMount, Show } from 'solid-js'
+import styles from "../../../assets/index.css";
+import { Bot, BotProps } from "@/components/Bot";
+import { BubbleParams } from "@/features/bubble/types";
+import { createSignal, onCleanup, onMount, Show } from "solid-js";
 
-export type FullProps = BotProps & BubbleParams
+export type FullProps = BotProps & BubbleParams;
 
-export const Full = (
-  props: FullProps,
-  { element }: { element: HTMLElement }
-) => {
-  const [isBotDisplayed, setIsBotDisplayed] = createSignal(false)
+export const Full = (props: FullProps, { element }: { element: HTMLElement }) => {
+  const [isBotDisplayed, setIsBotDisplayed] = createSignal(false);
 
   const launchBot = () => {
-    setIsBotDisplayed(true)
-  }
+    setIsBotDisplayed(true);
+  };
 
   const botLauncherObserver = new IntersectionObserver((intersections) => {
-    if (intersections.some((intersection) => intersection.isIntersecting))
-      launchBot()
-  })
+    if (intersections.some((intersection) => intersection.isIntersecting)) launchBot();
+  });
 
   onMount(() => {
-    botLauncherObserver.observe(element)
-  })
+    botLauncherObserver.observe(element);
+  });
 
   onCleanup(() => {
-    botLauncherObserver.disconnect()
-  })
+    botLauncherObserver.disconnect();
+  });
 
   return (
     <>
-      <style>
-        {styles}
-      </style>
+      <style>{styles}</style>
       <Show when={isBotDisplayed()}>
         <div
           style={{
-            'background-color': props.theme?.chatWindow?.backgroundColor || '#ffffff',
-            'height': props.theme?.chatWindow?.height ? `${props.theme?.chatWindow?.height.toString()}px` : '100vh',
-            'width': props.theme?.chatWindow?.width ? `${props.theme?.chatWindow?.width.toString()}px` : '100%',
-            'margin': '0px'
+            "background-color": props.theme?.chatWindow?.backgroundColor || "#ffffff",
+            height: "300px", // Sesuaikan tinggi chatbox sesuai kebutuhan Anda
+            width: "400px", // Sesuaikan lebar chatbox sesuai kebutuhan Anda
+            margin: "auto", // Membuat chatbox berada di tengah
+            "margin-top": "50px", // Sesuaikan sesuai kebutuhan
           }}
         >
           <Bot
@@ -52,9 +47,10 @@ export const Full = (
             fontSize={props.theme?.chatWindow?.fontSize}
             chatflowid={props.chatflowid}
             chatflowConfig={props.chatflowConfig}
-            apiHost={props.apiHost} />
+            apiHost={props.apiHost}
+          />
         </div>
       </Show>
     </>
-  )
-}
+  );
+};
